d275aeb74e6cbbe9e3ed448c22efc78d55c2a260
Andrew Lukashchuk <aluk@seas.upenn.edu>
2024-12-04 23:18:32 -0500

    dark mode

--- src/theme.js
+++ src/theme.js
@@ -3,10 +3,10 @@ import { createTheme } from '@mui/material/styles';
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#1976d2',
+      main: '#81c784',
     },
     secondary: {
-      main: '#dc004e',
+      main: '#4caf50',
     },
   },
   typography: {
